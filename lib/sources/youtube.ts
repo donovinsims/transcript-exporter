71cba88309fa1bdddb2a1a870f6c02f7f74b2dfb
donny <emaildonovin@gmail.com>
2026-02-28 12:06:21 -0600

    fix: resolve invalid regex in youtube.ts

--- lib/sources/youtube.ts
+++ lib/sources/youtube.ts
@@ -65,7 +65,7 @@ export async function fetchYoutubeTranscript(
 }
 
 function extractYoutubeId(url: string): string | null {
-    const regExp = /^.*(youtu.be\\/ | v\\/|u\\/\\w\\/|embed\\/ | watch\\?v =|\\& v=) ([^#\\&\\?] *).*/;
+    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     const match = url.match(regExp);
     return match && match[2].length === 11 ? match[2] : null;
 }
